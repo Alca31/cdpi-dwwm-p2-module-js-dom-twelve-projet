6bcb5f15c8dd83c652deab639021a70da7937b60
Valer1712 <dreptatevaler1712@gmail.com>
2025-09-25 17:41:02 +0200

    fin de lexo 4 debut exo5

--- 3 - dark mode/script.js
+++ 3 - dark mode/script.js
@@ -3,3 +3,17 @@ Nouvelles compétences:
 - forEach
 - querySelectorAll
 */
+
+let btn = document.querySelector(".btn");
+
+
+btn.addEventListener("click",function(){
+    //passer tout en darkmode
+    const allTags = document.querySelectorAll("*");
+     
+
+    allTags.forEach(function(tag){
+        tag.classList.toggle("darkmode");
+    })
+        
+});
--- 4 - menuburger/script.js
+++ 4 - menuburger/script.js
@@ -1,5 +1,11 @@
 
 // 1. Je recupere le burger
-const burger = document.querySelector(".burger");
+let burger = document.querySelector(".burger");
 // 2. Je recupere la fenetre modal
-const modal = document.querySelector(".modal");
+let modal = document.querySelector(".modal");
+
+burger.addEventListener("click",function(){
+     burger.classList.toggle("fa-x");
+    burger.classList.toggle("show-modal");
+    modal.classList.toggle("show-modal");
+});
