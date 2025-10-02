46bc475b62f895f44a50231455ff1863ca7be6d5
Valer1712 <dreptatevaler1712@gmail.com>
2025-10-02 14:37:17 +0200

    les douzes travaux accompli

--- 11 - todolist/script.js
+++ 11 - todolist/script.js
@@ -8,13 +8,40 @@ const form = document.querySelector("#form");
 // 2. Je recupere le conteneur des taches
 const listTasks = document.querySelector(".list-tasks");
 
+const todo = document.querySelector("#todo");
+
+
 // 3. Lorsque le formulaire est soumis
-form.addEventListener("submit",function(e){
-    
-    // ...
-    
-    
+form.addEventListener("submit", onAddTask);
+
+function onAddTask(event) {
+    event.preventDefault();
+
+    const postElement = `
+                <div class="task">
+                    <p>${todo.value}</p>
+                    <button class="btn-delete">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn-archive">
+                        <i class="fas fa-check-circle"></i>
+                    </button>
+                </div>
+    `;
+
+    listTasks.innerHTML += postElement;
+
+    const btnDelete = document.querySelector(".btn-delete");
+    const btnArchive = document.querySelector(".btn-archive");
+
+    btnDelete.addEventListener("click", function () {
+        btnDelete.parentElement.remove();
+    });
+
+    btnArchive.addEventListener("click", function () {
+        btnArchive.parentElement.classList.toggle("done");
+    });
+
     // Je reset le contenu du formulaire pour eviter que le texte reste une fois la tache crée (ergonomique)
     form.reset();
-});
-
+}
--- 12 - banniere localStorage/script.js
+++ 12 - banniere localStorage/script.js
@@ -4,3 +4,32 @@
  * getItem()
  * Fonctionnement de Stockage et des cookies
  */
+/*
+Nouvelles compétences:
+- querySelector // Selectionne le premier element seulement mieux que getElementsBy
+- HTMLElement.style 
+*/
+
+// 1. Je recupere le bouton "Accepter les cookies"
+
+const btn_accept = document.querySelector(".btn-accept");
+
+// 2. Je recupere la banniere de cookies
+
+let cookies = document.querySelector(".cookies");
+
+// 3. J'ecoute l'evenement "click" sur le bouton "Accepter les cookies"
+
+btn_accept.onclick = onClicSurBalise;
+function onClicSurBalise() {
+    cookies.classList.add("onclick");
+    sessionStorage.setItem("cookiesAccepted", "true");
+}
+
+function inStorage(){
+    if(sessionStorage.getItem("cookiesAccepted") === "true"){
+        cookies.classList.add("onclick");
+    }
+}
+
+inStorage();
